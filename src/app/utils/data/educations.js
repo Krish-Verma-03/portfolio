49009142f60d7987611b07ec33596b9d6ca4bb83
Science-lover-krish <93467650+Science-lover-krish@users.noreply.github.com>
2024-08-07 14:58:26 +0530

    hero section

---
+++ src/app/utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Technology in Computer Science",
+    duration: "2021 - Present",
+    institution: "Panipat Institute of Engineering & Technology",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Schooling",
+    duration: "2019 - 2021",
+    institution: "Arya Bal Bharti Public School",
+  },
+  {
+    id: 3,
+    title: "Secondary Schooling",
+    duration: "2009 - 2019",
+    institution: "Arya Bal Bharti Public School",
+  }
+]
---
+++ src/app/utils/data/experience.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Android Development Trainee',
+    company: "Cetpa Infotech Private Ltd.",
+    duration: "(July 2023 - October 2023)"
+  },
+  {
+    id: 2,
+    title: "Java Development",
+    company: "CodSoft",
+    duration: "(July 2023 - August 2023)"
+  }
+]
